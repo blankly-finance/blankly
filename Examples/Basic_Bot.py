c20794ca7bcdd4e40c2711d68999a8df3710c2de
Emerson <edove@vt.edu>
2021-04-29 14:39:49 -0400

    Beginner friendly comments.

--- Examples/Basic_Bot.py
+++ Examples/Basic_Bot.py
@@ -12,7 +12,7 @@ class Bot(Blankly.BlanklyBot):
         self.Ticker_Manager.create_ticker(callback=self.price_event)
 
         # Add a heartbeat example to report to GUI or main
-        self.update_state("Heartbeat", 0)
+        self.update_state("heartbeat", 0)
 
         # This will work on any supported exchange
         print("Interface call: " + str(self.Interface.get_fees()))
@@ -22,11 +22,13 @@ class Bot(Blankly.BlanklyBot):
             # Because this is direct, some exchanges have different commands, so this demo is wrapped in try/except
             print("Direct call: " + str(self.direct_calls.get_fees()))
         except AttributeError:
+            # If it doesn't have that function we just complain about it and move on
             print("Chosen exchange doesn't have that function")
 
         while True:
             # This demonstrates a way to change the state. This is just reporting to the main.
-            self.update_state("Heartbeat", self.get_state()["Heartbeat"] + 1)
+            current_heartbeat = self.get_state()["heartbeat"]
+            self.update_state("heartbeat", current_heartbeat + 1)
             time.sleep(1)
 
     def price_event(self, tick):
@@ -44,33 +46,21 @@ if __name__ == "__main__":
     Easily setup and run a model on any supported exchange
     """
 
-    # This creates an authenticated exchange. Now we can append models.
+    # This creates an authenticated exchange and chooses the first portfolio in the keys.json file
+    # You can use portfolio_name="my cool portfolio" if want a certain one
     portfolio = Blankly.Coinbase_Pro()  # You could also use Blankly.Binance()
-    # Imagine this:
-    #   Coinbase Pro <-- Choosing to assign this bot to this exchange
-    #   Kraken
-    #   Binance
 
     # Create the bot and add it to run as a coinbase_pro bitcoin model.
     bot = Bot()
-    portfolio.append_model(model=bot, coin_id="BTC-USD")
-    # Imagine this:
-    #   Coinbase Pro:
-    #       Bitcoin <-- Added to the data from this currency
-    #       Ethereum
-    #       Stellar
+    portfolio.append_model(model=bot, coin_id="BTC-USD", args=[])
 
-    # Begins running the main() function of the model on a different process
+    # This starts the main() function of the model and puts it on a different process (computer core)
+    # The main() function is in the class above
     portfolio.start_models(coin_id="BTC-USD")
-    # Imagine this:
-    #   Coinbase Pro:
-    #       Bitcoin <-- Bot <-- Asking to start
-    #       Ethereum
-    #       Stellar
 
     # Now other processes can be created or just continue with this one.
     while True:
-        # Print the state every 1 second
+        # Print the state from the model we just started every second
         state = portfolio.get_full_state("BTC-USD")
         Blankly.utils.pretty_print_JSON(state)
         time.sleep(1)
