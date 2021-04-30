6f0d8e51a5e135ce93db2c9ec06e559cc35d55af
Emerson <edove@vt.edu>
2021-04-29 22:38:23 -0400

    Binance sandbox boilerplate added.

--- Blankly/exchanges/Binance/Binance.py
+++ Blankly/exchanges/Binance/Binance.py
@@ -19,8 +19,10 @@
 from Blankly.exchanges.exchange import Exchange
 from Blankly.API_Interface import APIInterface as Interface
 import Blankly.auth_constructor
+import Blankly.utils.utils as utils
 
 from binance.client import Client
+import warnings
 
 
 class Binance(Exchange):
@@ -30,6 +32,11 @@ class Binance(Exchange):
 
         Exchange.__init__(self, "binance", defined_name)
 
+        preferences = utils.load_user_preferences()
+        if preferences["settings"]["use_sandbox"]:
+            warnings.warn("Binance sandbox authentication not yet supported")
+        else:
+            pass
         self.__calls = Client(api_key=auth[0], api_secret=auth[1],
                               tld=self.get_preferences()["settings"]["binance_tld"])
 
