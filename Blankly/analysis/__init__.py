84abb02880f99729945f99dea9a4eee6c48b1a88
Emerson <edove@vt.edu>
2021-05-09 20:03:31 -0400

    Fixed import error.

--- Blankly/analysis/__init__.py
+++ Blankly/analysis/__init__.py
@@ -1,1 +1,1 @@
-from basic_analysis import *
+from Blankly.analysis.basic_analysis import *
--- Blankly/analysis/basic_analysis.py
+++ Blankly/analysis/basic_analysis.py
@@ -1,10 +1,12 @@
 from statistics import mean
 
 
-def average(data):
+def average(data) -> float:
     """
     Find average of dataset
     Args:
         data: (list) A list containing the values to determine the mean
+    Returns:
+        float: This is the mean of the dataset
     """
     return mean(data)
