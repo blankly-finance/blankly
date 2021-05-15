a9cc2a810abea4b3723d9fb9a01cb5b5f39c9d96
Emerson <edove@vt.edu>
2021-05-14 21:58:47 -0400

    Starting github actions workflow.

--- Blankly/utils/purchases/order.py
+++ Blankly/utils/purchases/order.py
@@ -1,7 +1,23 @@
-import abc
+"""
+    Inherited class to abstract the similarities between order types
+    Copyright (C) 2021  Emerson Dove
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 
-class Order(abc.ABC):
+class Order:
     def __init__(self, response, order, interface):
         """
         Limit Order:
---
+++ Blankly/utils/tests/tests.py
@@ -0,0 +1,5 @@
+import Blankly
+
+
+if __name__ == "__main__":
+    print("Hello world")
