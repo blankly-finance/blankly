59e04678bb9fe5ef4c24c04039257cb84875aed8
Brandon Fan <bxfan@umich.edu>
2021-05-16 10:42:42 -0500

    update strategy base to use direct ticker

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -2,14 +2,18 @@ import Blankly
 
 class Strategy(Blankly.Bot):
     def __init__(self):
+        """
+        Initializes Strategy base
+        """
         super.__init__()
         self.price_event_funcs = []
+        self.orderbook_event_funcs = []
     def main(self, args):
-        self.Ticker_manager.create_ticker(callback=self.price_event)
-
-    def price_event(self, tick):
-        for func in self.price_event_funcs:
-            func(tick, self.currency_pair)
+        self.ticker = self.Ticker_manager.create_ticker(callback=self.price_event)
+        self.orderbook = self.Orderbook_manager.create_ticker(callback=self.price_event)
 
     def add_price_event(self, func, resolution):
-        self.price_event_funcs.append(Blankly.Scheduler(func, resolution))
+        self.ticker.append_callback(Blankly.Scheduler(func, resolution))
+
+    def add_orderbook_event(self, func):
+        self.orderbook.append_callback(func)
