7cce849626d1b7918566667e9bd2e5d167e26e84
Brandon Fan <bxfan@umich.edu>
2021-05-16 10:45:38 -0500

    update orderbook callbacks

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -3,7 +3,7 @@ import Blankly
 class Strategy(Blankly.Bot):
     def main(self, args):
         self.ticker = self.Ticker_manager.create_ticker(callback=self.price_event)
-        self.orderbook = self.Orderbook_manager.create_ticker(callback=self.price_event)
+        self.orderbook = self.Orderbook_manager.create_orderbook(callback=self.orderbook_event)
 
     def add_price_event(self, func, resolution):
         self.ticker.append_callback(Blankly.Scheduler(func, resolution))
