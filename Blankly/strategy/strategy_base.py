4933899d04215bedb3d1435f3d635af9d68580bd
Brandon Fan <bxfan@umich.edu>
2021-05-16 10:43:34 -0500

    remove unnecessary __init__

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -1,13 +1,6 @@
 import Blankly
 
 class Strategy(Blankly.Bot):
-    def __init__(self):
-        """
-        Initializes Strategy base
-        """
-        super.__init__()
-        self.price_event_funcs = []
-        self.orderbook_event_funcs = []
     def main(self, args):
         self.ticker = self.Ticker_manager.create_ticker(callback=self.price_event)
         self.orderbook = self.Orderbook_manager.create_ticker(callback=self.price_event)
