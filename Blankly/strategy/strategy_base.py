2660b1b629c61957e5010700308d978ac15dfa73
Brandon Fan <bxfan@umich.edu>
2021-05-16 10:08:01 -0500

    add initial strategy

---
+++ Blankly/strategy/strategy_base.py
@@ -0,0 +1,15 @@
+import Blankly
+
+class Strategy(Blankly.Bot):
+    def __init__(self):
+        super.__init__()
+        self.price_event_funcs = []
+    def main(self, args):
+        self.Ticker_manager.create_ticker(callback=self.price_event)
+
+    def price_event(self, tick):
+        for func in self.price_event_funcs:
+            func(tick, self.coin)
+
+    def add_price_event(self, func, resolution):
+        self.price_event_funcs.append(Blankly.Scheduler(func, resolution))
