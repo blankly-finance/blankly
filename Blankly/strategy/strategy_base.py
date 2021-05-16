d4e54293fe588e24ce84ab439133f17e90daffb0
Brandon Fan <bxfan@umich.edu>
2021-05-16 10:47:45 -0500

    add documentation

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -5,8 +5,19 @@ class Strategy(Blankly.Bot):
         self.ticker = self.Ticker_manager.create_ticker(callback=self.price_event)
         self.orderbook = self.Orderbook_manager.create_orderbook(callback=self.orderbook_event)
 
-    def add_price_event(self, func, resolution):
-        self.ticker.append_callback(Blankly.Scheduler(func, resolution))
+    def add_price_event(self, callback, resolution):
+        """
+        Add Orderbook Event
+        Args:
+            callback: The price event callback that will be added to the current ticker and run at the proper resolution
+            resolution: The resolution that the callback will be run
+        """
+        self.ticker.append_callback(Blankly.Scheduler(callback, resolution))
 
-    def add_orderbook_event(self, func):
-        self.orderbook.append_callback(func)
+    def add_orderbook_event(self, callback):
+        """
+        Add Orderbook Event
+        Args:
+            callback: The Orderbook callback that will be added to the current ticker orderbook
+        """
+        self.orderbook.append_callback(callback)
