622487c2cff409cc9373bd648758fa2519d6ea63
Emerson <edove@vt.edu>
2021-05-18 16:06:02 -0400

    Changing some of brandon's code.

--- Blankly/strategy/strategy_base.py
+++ Blankly/strategy/strategy_base.py
@@ -1,30 +1,51 @@
+"""
+    User abstraction for Blankly Bot
+    Copyright (C) 2021  Emerson Dove, Brandon Fan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import Blankly
 from Blankly.strategy.order import Order
 from Blankly.utils.time_builder import time_interval_to_seconds
 
 
-class Strategy(Blankly.BlanklyBot):
-    def __init__(self):
-        super().__init__()
+class Strategy:
+    def __init__(self, exchange, currency_pair='BTC-USD'):
+        self.exchange = exchange
         self.price_funcs = []
         self.order_funcs = []
+        self.Ticker_Manager = Blankly.TickerManager(self.exchange.get_type(), currency_pair)
+        self.Orderbook_Manager = Blankly.OrderbookManager(self.exchange.get_type(), currency_pair)
+        self.currency_pair = currency_pair
+        self.Interface = exchange.get_interface()
 
-    def main(self, args):
-        pass
-
-    def add_price_event(self, callback, resolution):
+    def add_price_event(self, callback, currency_pair, resolution):
         """
         Add Orderbook Event
         Args:
             callback: The price event callback that will be added to the current ticker and run at the proper resolution
+            currency_pair: Currency pair to create the price event for
             resolution: The resolution that the callback will be run - in seconds
         """
         if time_interval_to_seconds(resolution) < 60:
             # since it's less than 60, we will just use the websocket feed
-            self.Ticker_Manager.append_callback(self._price_event_websocket(callback))
+            self.Ticker_Manager.append_callback(self.__price_event_websocket(callback))
         else:
             # use the API 
-            Blankly.Scheduler(self._price_event_rest, resolution, callback=callback)
+            Blankly.Scheduler(self.__price_event_rest, resolution, callback=callback)
 
     def add_orderbook_event(self, callback):
         """
@@ -32,9 +53,10 @@ class Strategy(Blankly.BlanklyBot):
         Args:
             callback: The price event callback that will be added to the current ticker and run at the proper resolution
         """
-        self.Orderbook_Manager.append_callback(self._orderbook_event)
+        self.Orderbook_Manager.append_callback(self.__orderbook_event)
 
-    def _price_event_rest(self, callback):
+    def __price_event_rest(self, **kwargs):
+        callback = kwargs['callback']
         price = self.Interface.get_price(self.currency_pair)
         order = callback(price, self.currency_pair)
         if order is not None:
@@ -46,12 +68,12 @@ class Strategy(Blankly.BlanklyBot):
                 return self.Interface.market_order(self.currency_pair, order.side, order.price, order.size)
         return
 
-    def _price_event_websocket(self, callback):
+    def __price_event_websocket(self, callback):
         price = self.Ticker_Manager.get_most_recent_tick()
         callback(price, self.currency_pair)
         return
 
-    def _orderbook_event(self, callback):
+    def __orderbook_event(self, callback):
         orderbook = self.Ticker_Manager.get_most_recent_tick()
         order = callback(orderbook, self.currency_pair)
         if order is not None:
---
+++ Blankly/strategy/strategy_usage_example.py
@@ -0,0 +1,24 @@
+import Blankly
+from Blankly.strategy.strategy_base import Strategy
+
+prices = []
+
+
+def price_event(price, currency_pair):
+    prices.append(price)
+    sma50 = Blankly.analysis.calculate_sma(price, window=10)
+    if price > sma50:
+        interface.market_order(currency_pair, 'buy', 10)
+    else:
+        interface.market_order(currency_pair, 'sell', 10)
+
+
+if __name__ == "__main__":
+    # Authenticate coinbase pro strategy
+    coinbase_pro = Blankly.Coinbase_Pro()
+    interface = coinbase_pro.get_interface()
+    coinbase_strategy = Strategy(coinbase_pro)
+
+    coinbase_strategy.add_price_event(price_event, currency_pair='BTC-USD', resolution='15m')
+    coinbase_strategy.add_price_event(price_event, currency_pair='XLM-USD', resolution='15m')
+    coinbase_strategy.add_price_event(price_event, currency_pair='COMP-USD', resolution='15m')
