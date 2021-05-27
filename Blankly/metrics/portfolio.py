b458dbe9ba1582301e55a9f3afffa223fad33fd1
Brandon Fan <bxfan@umich.edu>
2021-05-27 13:43:58 -0500

    add portfolio metrics

---
+++ Blankly/metrics/portfolio.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+
+def cagr(start_value, end_value, years):
+    return (end_value / start_value) ** (1 / years)
+
+def sortino(returns, days=255, risk_free_rate=None):
+    returns = pd.Series(returns)
+    if risk_free_rate:
+        mean = returns.mean() * days - risk_free_rate
+    else:
+        mean = returns.mean() * days
+    std_neg = returns[returns < 0].std() * np.sqrt(days)
+    return mean / std_neg
+
+def sharpe(returns, days=255, risk_free_rate=None):
+    returns = pd.Series(returns)
+    if risk_free_rate:
+        mean = returns.mean() * days - risk_free_rate
+    else:
+        mean = returns.mean() * days
+    std = returns.std() * np.sqrt(days)
+    return mean / std
+
+def calmar(returns, days=255):
+    return_series = pd.Series(returns)
+    return return_series.mean() * days / abs(max_drawdown(return_series))
+
+def volatility(returns):
+    return np.std(returns)
+
+def variance(returns):
+    return np.var(returns)
+
+def beta(returns, market_base_returns):
+    m = np.matrix([returns, market_base_returns])
+    return np.cov(m)[0][1] / np.std(market_base_returns)
+
+def var(returns, alpha: float):
+    returns_sorted = np.sort(returns)
+    index = int(alpha * len(returns_sorted))
+    return abs(returns_sorted[index])
+
+def cvar(returns, alpha):
+    returns_sorted = np.sort(returns)
+    index = int(alpha * len(returns_sorted))
+    sum_var = returns_sorted[0]
+    for i in range(1, index):
+        sum_var += returns_sorted[i]
+    return abs(sum_var / index)
+
+def max_drawdown(returns):
+    return_series = pd.Series(returns)
+    comp_ret = (return_series+1).cumprod()
+    peak = comp_ret.expanding(min_periods=1).max()
+    dd = (comp_ret/peak)-1
+    return dd.min()
+
