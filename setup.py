34f62056c8edca3706638c58f11f978cb0a00865
Emerson <edove@vt.edu>
2021-06-14 22:11:50 -0400

    Increment version for release.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='Blankly',  # How you named your package folder (MyLib)
     packages=find_packages(),
     # packages=['Blankly'],  # Potentially should be the same thing as name
-    version='v0.1.11-alpha',
+    version='v0.1.12-alpha',
     license='lgpl-3.0',  # Licenses: https://help.github.com/articles/licensing-a-repository
     description='Cryptocurrency bot development platform',  # Give a short description about your library
     long_description=long_description,
