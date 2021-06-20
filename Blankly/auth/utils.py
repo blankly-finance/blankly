e998fd337e2c8b1f09d596f3e79733e18289e0fa
Emerson Dove <edove@vt.edu>
2021-06-20 16:25:18 -0700

    Licenses fix, file closing fix, standards fix.

--- Blankly/auth/utils.py
+++ Blankly/auth/utils.py
@@ -1,3 +1,22 @@
+"""
+    Utils for constructing authentication
+    Copyright (C) 2021  Emerson Dove, Arun Annamalai
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import json
 import warnings
 
@@ -5,7 +24,9 @@ import warnings
 def load_json(keys_file):
     try:
         f = open(keys_file)
-        return json.load(f)
+        contents = json.load(f)
+        f.close()
+        return contents
     except FileNotFoundError:
         raise FileNotFoundError("Make sure a Keys.json file is placed in the same folder as the project working "
                                 "directory!")
