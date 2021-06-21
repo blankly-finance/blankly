15b6cfd3cd9a8fe585239cfd83f5b6da3d62ab19
Arun Annamalai <aannamal@umich.edu>
2021-06-20 21:38:24 -0700

    small test case fix

--- tests/exchanges/Binance/test_Binance_Interface.py
+++ tests/exchanges/Binance/test_Binance_Interface.py
@@ -55,7 +55,7 @@ def test_get_exchange(binance_interface: BinanceInterface) -> None:
 
 def test_get_account(binance_interface: BinanceInterface) -> None:
     resp = binance_interface.get_account()
-    assert type(resp) is dict
+    assert type(resp) is list
 
     found_btc = False
     for asset in resp:
