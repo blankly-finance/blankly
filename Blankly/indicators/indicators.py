e36f19419c517cc4088eaf98928c30b86c2b060e
Brandon Fan <brandonfan1256@gmail.com>
2021-06-22 14:00:25 -0500

    add indicator tests

--- Blankly/indicators/indicators.py
+++ Blankly/indicators/indicators.py
@@ -10,11 +10,13 @@ def bbands(data, period=14, stddev=2):
     return ti.bbands(data, period, stddev)
 
 
-def wad(data, period=50, use_series=False):
-    if check_series(data):
+def wad(high_data, low_data, close_data, use_series=False):
+    if check_series(high_data) or check_series(low_data) or check_series(close_data):
         use_series = True
-    data = convert_to_numpy(data)
-    wad = ti.wad(data, period)
+    high_data = convert_to_numpy(high_data)
+    low_data = convert_to_numpy(low_data)
+    close_data = convert_to_numpy(close_data)
+    wad = ti.wad(high_data, low_data, close_data)
     return pd.Series(wad) if use_series else wad
 
 
@@ -26,21 +28,23 @@ def wilders(data, period=50, use_series=False):
     return pd.Series(wilders) if use_series else wilders
 
 
-def willr(data, period=50, use_series=False):
-    if check_series(data):
-        use_series = True
-    data = convert_to_numpy(data)
-    willr = ti.willr(data, period)
-    return pd.Series(willr) if use_series else willr
-
-
-def true_range(high_data, low_data, close_data, period=50, use_series=False):
+def willr(high_data, low_data, close_data, period=50, use_series=False):
     if check_series(high_data) or check_series(low_data) or check_series(close_data):
         use_series = True
     high_data = convert_to_numpy(high_data)
     low_data = convert_to_numpy(low_data)
     close_data = convert_to_numpy(close_data)
-    tr = ti.tr(high_data, low_data, close_data, period=period)
+    willr = ti.willr(high_data, low_data, close_data, period)
+    return pd.Series(willr) if use_series else willr
+
+
+def true_range(high_data, low_data, close_data, use_series=False):
+    if check_series(high_data) or check_series(low_data) or check_series(close_data):
+        use_series = True
+    high_data = convert_to_numpy(high_data)
+    low_data = convert_to_numpy(low_data)
+    close_data = convert_to_numpy(close_data)
+    tr = ti.tr(high_data, low_data, close_data)
     return pd.Series(tr) if use_series else tr
 
 
