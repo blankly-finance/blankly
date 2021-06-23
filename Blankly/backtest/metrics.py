51727ab63cca4bb666017317084e6eddda9530ea
Brandon Fan <brandonfan1256@gmail.com>
2021-06-23 16:07:07 -0500

    add initial scaffolding for backtest metrics

---
+++ Blankly/backtest/metrics.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import Blankly.metrics as metrics
+
+
+def cagr(backtest_data):
+    # TODO
+    return metrics.cagr()
+def cum_returns(backtest_data):
+    # TODO
+    return metrics.cum_returns()
+
+def sortino(backtest_data):
+    # TODO
+    return metrics.sortino()
+
+def sharpe(backtest_data):
+    # TODO
+    return metrics.sharpe()
+def calmar(backtest_data):
+    # TODO
+    return metrics.calmar()
+def volatility(backtest_data):
+    # TODO
+    return metrics.volatility()
+
+def variance(backtest_data):
+    # TODO
+    return metrics.variance()
+
+def beta(backtest_data):
+    # TODO
+    return metrics.beta()
+
+def var(backtest_data):
+    # TODO
+    return metrics.var()
+
+def cvar(backtest_data):
+    # TODO
+    return metrics.cvar()
+
+def max_drawdown(backtest_data):
+    # TODO
+    return metrics.max_drawdown()
