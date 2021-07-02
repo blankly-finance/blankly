cdf1146c4307fbfdc5032d247db0cfba4c13b9e1
Brandon Fan <brandonfan1256@gmail.com>
2021-07-01 19:46:06 -0500

    create backtesting metrics

--- Blankly/backtest/metrics.py
+++ Blankly/backtest/metrics.py
@@ -1,45 +1,64 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import pytz
+from dateutil.relativedelta import relativedelta
 import Blankly.metrics as metrics
 
 
 def cagr(backtest_data):
-    # TODO
-    return metrics.cagr()
+    account_values = backtest_data['resampled_account_value']
+    start = datetime.fromtimestamp(account_values[0]['time'], tz=pytz.utc)
+    end = datetime.fromtimestamp(account_values[-1]['time'], tz=pytz.utc)
+    years = relativedelta(start, end).years
+    return metrics.cagr(account_values[0], account_values[-1], years)
 def cum_returns(backtest_data):
-    # TODO
-    return metrics.cum_returns()
+    account_values = backtest_data['resampled_account_value']
+    return metrics.cum_returns(account_values[0], account_values[-1])
 
 def sortino(backtest_data):
-    # TODO
-    return metrics.sortino()
+    # TODO: Need to pass in the specific resolution
+    # Defaulting to 1d
+    returns = backtest_data['returns']['value']
+    return metrics.sortino(returns)
 
 def sharpe(backtest_data):
-    # TODO
-    return metrics.sharpe()
+    # TODO: Need to pass in the specific resolution
+    # Defaulting to 1d
+    returns = backtest_data['returns']['value']
+    return metrics.sharpe(returns)
 def calmar(backtest_data):
-    # TODO
-    return metrics.calmar()
+    # TODO: Need to pass in the specific resolution
+    # Defaulting to 1d
+    returns = backtest_data['returns']['value']
+    return metrics.calmar(returns)
 def volatility(backtest_data):
-    # TODO
-    return metrics.volatility()
+    returns = backtest_data['returns']['value']
+    return metrics.volatility(returns)
 
 def variance(backtest_data):
-    # TODO
-    return metrics.variance()
+    returns = backtest_data['returns']['value']
+    return metrics.variance(returns)
 
 def beta(backtest_data):
-    # TODO
-    return metrics.beta()
+    # TODO: Need to pass in the specific resolution
+    # Defaulting to 1d
+    # Need to get some sort of baseline for this one...
+    # Use SP500 as default for all of them (can we get this data?)
+    # Or pick one of the assets as a baseline
+    returns = backtest_data['returns']['value']
+    return metrics.beta(returns)
 
 def var(backtest_data):
-    # TODO
-    return metrics.var()
+    returns = backtest_data['returns']['value']
+    account_values = backtest_data['resampled_account_value']
+    return metrics.var(account_values[0], returns, 0.95)
 
 def cvar(backtest_data):
-    # TODO
-    return metrics.cvar()
+    returns = backtest_data['returns']['value']
+    account_values = backtest_data['resampled_account_value']
+    return metrics.cvar(account_values[0], returns, 0.95)
 
 def max_drawdown(backtest_data):
-    # TODO
+    returns = backtest_data['returns']['value']
     return metrics.max_drawdown()
