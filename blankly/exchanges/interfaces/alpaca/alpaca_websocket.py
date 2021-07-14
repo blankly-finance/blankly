8946ce13de47432940112835cccefabd5ed5521c
Emerson <edove@vt.edu>
2021-07-14 18:21:39 -0400

    Added auth caching and new custom alpaca definition.

--- blankly/exchanges/interfaces/alpaca/alpaca_websocket.py
+++ blankly/exchanges/interfaces/alpaca/alpaca_websocket.py
@@ -16,29 +16,54 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import websocket
-from blankly.exchanges.abc_exchange_websocket import ABCExchangeWebsocket
-import blankly.exchanges.interfaces.binance.binance_websocket_utils as websocket_utils
-import collections
-import json
 import blankly
 import threading
+import json
+import ssl
+import msgpack
 import traceback
+from blankly.exchanges.abc_exchange_websocket import ABCExchangeWebsocket
+import blankly.exchanges.interfaces.coinbase_pro.coinbase_pro_websocket_utils as websocket_utils
+import collections
+
+from websocket import create_connection
+
+
+def create_ticker_connection(symbol, url, channel):
+    ws = create_connection(url, sslopt={"cert_reqs": ssl.CERT_NONE}, header={'Content-Type': 'application/msgpack'})
+
+    ws.send(msgpack.packb({
+        'action': 'auth',
+        'key': '<>',
+        'secret': '<>'
+    }))
+    print(msgpack.unpackb(ws.recv()))
+    ws.send(
+        msgpack.packb({
+            'action': 'subscribe',
+            channel: tuple([symbol]),
+        })
+    )
+    return ws
+
+
+# 'action': 'subscribe',
+# 'trades': tuple(['AAPL']),
+# 'quotes': tuple(['AAPL']),
+# 'bars': tuple(['AAPL']),
 
 
 class Tickers(ABCExchangeWebsocket):
-    def __init__(self, symbol, stream, log=None, initially_stopped=False,
-                 WEBSOCKET_URL="wss://api.alpaca.markets/stream"):
+    def __init__(self, symbol, stream, log=None,
+                 pre_event_callback=None, initially_stopped=False, WEBSOCKET_URL="wss://ws-feed.pro.coinbase.com"):
         """
         Create and initialize the ticker
         Args:
-            symbol: Currency to initialize on such as "btcusdt"
-            stream: Stream to use, such as "depth" or "trade"
+            symbol: Currency to initialize on such as "BTC-USD"
             log: Fill this with a path to a log file that should be created
-            initially_stopped: Keep the websocket closed when the websocket is created
             WEBSOCKET_URL: Default websocket URL feed.
         """
-        self.__id = symbol
+        self.__symbol = symbol
         self.__stream = stream
         self.__logging_callback, self.__interface_callback, log_message = websocket_utils.switch_type(stream)
 
@@ -59,11 +84,8 @@ class Tickers(ABCExchangeWebsocket):
         self.__response = None
         self.__most_recent_tick = None
         self.__most_recent_time = None
-        # Create the thread object here
-        self.__thread = threading.Thread(target=self.read_websocket)
         self.__callbacks = []
-        # This is created so that we know when a message has come back that we're waiting for
-        self.__message_count = 0
+        self.__pre_event_callback = pre_event_callback
 
         # Reload preferences
         self.__preferences = blankly.utils.load_user_preferences()
@@ -80,15 +102,12 @@ class Tickers(ABCExchangeWebsocket):
         Restart websocket if it was asked to stop.
         """
         if self.ws is None:
-            self.ws = websocket.WebSocketApp(self.URL,
-                                             on_open=self.on_open,
-                                             on_message=self.on_message,
-                                             on_error=self.on_error,
-                                             on_close=self.on_close)
-            self.__thread = threading.Thread(target=self.read_websocket)
-            self.__thread.start()
+            self.ws = create_ticker_connection(self.__symbol, self.URL, self.__stream)
+            self.__response = self.ws.recv()
+            thread = threading.Thread(target=self.read_websocket)
+            thread.start()
         else:
-            if self.__thread.is_alive():
+            if self.ws.connected:
                 print("Already running...")
                 pass
             else:
@@ -97,60 +116,71 @@ class Tickers(ABCExchangeWebsocket):
                 self.start_websocket()
 
     def read_websocket(self):
-        # Main thread to sit here and run
-        self.ws.run_forever()
-        # This repeats the close behavior just in case something happens
+        # This is unique because coinbase first sends the entire orderbook to use
+        if self.__pre_event_callback is not None and self.__stream == "level2":
+            received_string = json.loads(self.ws.recv())
+            if received_string['type'] == 'snapshot':
+                try:
+                    self.__pre_event_callback(received_string)
+                except:
+                    traceback.print_exc()
 
-    def on_message(self, ws, message):
-        self.__message_count += 1
-        message = json.loads(message)
-        try:
-            self.__most_recent_time = message['E']
-            self.__time_feed.append(self.__most_recent_time)
-            self.__most_recent_tick = message
-            self.__ticker_feed.append(self.__most_recent_tick)
-            # Run callbacks on message
-            if self.__log:
-                if self.__message_count % 100 == 0:
-                    self.__file.close()
-                    self.__file = open(self.__filePath, 'a')
-                line = self.__logging_callback(message)
-                self.__file.write(line)
-
-            interface_message = self.__interface_callback(message)
+        counter = 0
+        # TODO port this to "WebSocketApp" found in the websockets documentation
+        while self.ws.connected:
+            # In case the user closes while its reading from the websocket, this will let it expire
+            persist_connected = self.ws.connected
+            received_string = self.ws.recv()
+            received = json.loads(received_string)
             for i in self.__callbacks:
-                i(interface_message)
-        except KeyError:
-            try:
-                if message['result'] is None:
-                    self.__response = message
-            except KeyError:
-                traceback.print_exc()
-
-    def on_error(self, ws, error):
-        print(error)
-
-    def on_close(self, ws):
-        # This repeats the close behavior just in case something happens
-        pass
-
-    def on_open(self, ws):
-        request = """
-        {
-            "action": "listen",
-            "data": {
-                "streams": [\"""" + self.__stream + """"\"]
-            }
-        }
-        """
-        ws.send(request)
+                i(received)
+            # try:
+            #     received_string = self.ws.recv()
+            #     received = json.loads(received_string)
+            #     # Modify time to use epoch
+            #     self.__most_recent_time = blankly.utils.epoch_from_ISO8601(received["time"])
+            #     received["time"] = self.__most_recent_time
+            #     self.__time_feed.append(self.__most_recent_time)
+            #     self.__most_recent_tick = received
+            #     self.__ticker_feed.append(received)
+            #
+            #     if self.__log:
+            #         if counter % 100 == 0:
+            #             self.__file.close()
+            #             self.__file = open(self.__filePath, 'a')
+            #         line = self.__logging_callback(received)
+            #         self.__file.write(line)
+            #
+            #     # Manage price events and fire for each manager attached
+            #     interface_message = self.__interface_callback(received)
+            #     try:
+            #         for i in self.__callbacks:
+            #             i(interface_message)
+            #     except Exception:
+            #         traceback.print_exc()
+            #
+            #     counter += 1
+            # except Exception as e:
+            #     if persist_connected:
+            #         traceback.print_exc()
+            #         pass
+            #     else:
+            #         traceback.print_exc()
+            #         print("Error reading ticker websocket for " + self.__id + " on " +
+            #               self.__stream + ": attempting to re-initialize")
+            #         # Give a delay so this doesn't eat up from the main thread if it takes many tries to initialize
+            #         time.sleep(2)
+            #         self.ws.close()
+            #         self.ws = create_ticker_connection(self.__id, self.URL, self.__stream)
+            #         # Update response
+            #         self.__response = self.ws.recv()
 
     """ Required in manager """
     def is_websocket_open(self):
-        return self.__thread.is_alive()
+        return self.ws.connected
 
     def get_currency_id(self):
-        return self.__id
+        return self.__symbol
 
     """ Required in manager """
     def append_callback(self, obj):
@@ -180,11 +210,20 @@ class Tickers(ABCExchangeWebsocket):
 
     """ Required in manager """
     def close_websocket(self):
-        if self.__thread.is_alive():
+        if self.ws.connected:
             self.ws.close()
         else:
-            print("Websocket for " + self.__id + '@' + self.__stream + " is already closed")
+            print("Websocket for " + self.__symbol + ' on channel ' + self.__stream + " is already closed")
 
     """ Required in manager """
     def restart_ticker(self):
         self.start_websocket()
+
+
+def trade_updates(update):
+    print(update)
+
+
+if __name__ == "__main__":
+    ticker = Tickers('AAPL', 'trade_updates', WEBSOCKET_URL="wss://stream.data.alpaca.markets/v2/iex")
+    ticker.append_callback(trade_updates)
