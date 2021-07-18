d72f6ad735122ea1c86172b7f15815ca9ffff62c
Emerson <edove@vt.edu>
2021-07-18 00:07:22 -0400

    Fixed paper trade failure.

--- blankly/exchanges/interfaces/paper_trade/paper_trade.py
+++ blankly/exchanges/interfaces/paper_trade/paper_trade.py
@@ -26,7 +26,7 @@ class PaperTrade(Exchange):
         # Giving the preferences path as none allows us to create a default
         Exchange.__init__(self, "paper_trade", "", None, None)
 
-        self.Interface = PaperTradeInterface(authenticated_exchange.get_interface(), initial_account_values)
+        self.interface = PaperTradeInterface(authenticated_exchange.get_interface(), initial_account_values)
         self.calls = authenticated_exchange.get_direct_calls()
 
     """
@@ -38,14 +38,14 @@ class PaperTrade(Exchange):
         Should be implemented per-class because it requires different types of interaction with each exchange.
         """
         # TODO Populate this with useful information
-        return self.Interface.get_account(symbol)
+        return self.interface.get_account(symbol)
 
     def get_exchange_state(self):
         """
         Exchange state is the external properties for the exchange block
         """
         # TODO Populate this with useful information
-        return self.Interface.get_fees()
+        return self.interface.get_fees()
 
     def get_direct_calls(self):
         return self.calls
