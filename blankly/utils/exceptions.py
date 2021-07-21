7869493a0baeabb567c129c7049a30440e24550f
Emerson Dove <edove@vt.edu>
2021-07-21 16:46:37 -0400

    Update legacy license header.

--- blankly/utils/exceptions.py
+++ blankly/utils/exceptions.py
@@ -1,16 +1,19 @@
 """
-    Exceptions definitions for blankly
-    Copyright (c) 2021 Emerson Dove
+    Exception definitions for blankly
+    Copyright (C) 2021 Emerson Dove
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 
