ed02c896d8fcb8cb75f738a5c103c164555d2adc
Emerson Dove <edove@vt.edu>
2021-08-12 22:12:52 -0400

    Build the deployment mechanisms using simple run demo command.

--- blankly/deployment/cli.py
+++ blankly/deployment/cli.py
@@ -17,6 +17,19 @@
 """
 
 import argparse
+import warnings
+
+from blankly.utils.utils import load_json_file
+import os
+import platform
+
+
+def add_path_arg(arg_parser):
+    arg_parser.add_argument('path',
+                            metavar='path',
+                            type=str,
+                            help='Path to the directory containing the blankly enabled folder.')
+
 
 parser = argparse.ArgumentParser(description='Blankly CLI & deployment tool.')
 
@@ -29,11 +42,12 @@ init_parser.set_defaults(which='init')
 deploy_parser = subparsers.add_parser('deploy', help='Sub command to deploy the model.')
 deploy_parser.set_defaults(which='deploy')
 
-deploy_parser.add_argument('path',
-                           metavar='path',
-                           type=str,
-                           nargs='?',
-                           help='Path to the directory containing the blankly enabled deploy code.')
+add_path_arg(deploy_parser)
+
+run_parser = subparsers.add_parser('run', help='Mimic the run mechanism used in blankly deployment.')
+run_parser.set_defaults(which='run')
+
+add_path_arg(run_parser)
 
 
 def main():
@@ -43,11 +57,45 @@ def main():
     except KeyError:
         parser.print_help()
         return
-
     if which == 'deploy':
-        pass
+        if args['path'] is None:
+            deploy_parser.print_help()
+        else:
+            pass
     elif which == 'init':
         pass
+    elif which == 'run':
+        if args['path'] is None:
+            run_parser.print_help()
+        else:
+            current_directory = os.getcwd()
+            blankly_folder = os.path.join(current_directory, args['path'])
+            deployment_location = os.path.join(blankly_folder, 'deploy.json')
+            try:
+                # Find where the user specified their working directory and migrate to that
+                deployment_dict = load_json_file(deployment_location)
+
+                # Set the working directory to match the deployment dictionary
+                os.chdir(os.path.join(blankly_folder, deployment_dict['working_directory']))
+
+                current_version = platform.python_version_tuple()
+                current_version = current_version[0] + "." + current_version[1]
+
+                if current_version != str(deployment_dict['python_version']):
+                    warn_string = f"Specified python version different than current interpreter. Using version " \
+                                  f"{platform.python_version()}. The specified version will be followed on full" \
+                                  f" deployment."
+                    warnings.warn(warn_string)
+
+                if 'requirements' in deployment_dict and deployment_dict['requirements'] is not None:
+                    warning_string = "Requirements specified but not installed in this test script. Install " \
+                                     "manually if needed."
+                    warnings.warn(warning_string)
+
+                print(os.path.join(blankly_folder, deployment_dict['main_script']))
+                exec(open(os.path.join(blankly_folder, deployment_dict['main_script'])).read())
+            except FileNotFoundError:
+                FileNotFoundError(f"Deployment json not found in location {deployment_location}.")
 
 
 main()
