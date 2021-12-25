f46aa1931aa849c99833f2235f70f65aab62f232
aydingokce <aydingokce@vt.edu>
2021-12-25 14:15:55 -0500

    finished api

--- blankly/exchanges/interfaces/ftx/ftx_api.py
+++ blankly/exchanges/interfaces/ftx/ftx_api.py
@@ -60,12 +60,225 @@ class API:
         except ValueError:
             response.raise_for_status()
             raise
-            
+
         else:
             if not data['success']:
                 raise Exception(data['error'])
             else:
                 return data['success']
+            
+
+    def list_markets(self) -> List[dict]:
+        return self._signed_get('markets')
+
+    def list_futures(self) -> List[dict]:
+        return self._signed_get('futures')
+
+    def get_orderbook(self, market: str, depth: int = None) -> dict:
+        return self._signed_get(f'markets/{market}/orderbook', {'depth': depth})
+
+    def get_trades(self, market: str) -> dict:
+        return self._signed_get(f'markets/{market}/trades')
+
+    def get_account_info(self) -> dict:
+        return self._signed_get('account')
+
+    def get_open_orders(self, market: str = None) -> List[dict]:
+        return self._signed_get('orders', {'market': market})
+
+    def get_fills(self) -> List[dict]:
+        return self._signed_get('fills')
+
+    def get_balances(self) -> List[dict]:
+        return self._signed_get('wallet/balances')
+
+    def get_deposit_addresses(self, ticker: str) -> dict:
+        return self._signed_get(f'wallet/deposit_addresses/{ticker}')
+    
+    def get_positions(self, display_price_avg: bool = False) -> List[dict]:
+        return self._signed_get('positions', {'showAvgPrice': show_avg_price})
+
+    def get_specific_position(self, pos_name: str, display_price_avg: bool = False) -> dict:
+        filtered = filter(lambda pos: pos['future'] == pos_name, self.get_positions(display_price_avg))
+        return next(filtered, None)
+
+    def get_order_history(
+        self, 
+        market:str = None, 
+        side: str = None, 
+        order_type: str = None, 
+        interval_start: float = None, 
+        interval_end: float = None
+        ) -> List[dict]:
+        return self._get(f'orders/history', {
+            'market': market, 
+            'side': side, 
+            'orderType': order_type, 
+            'start_time': interval_start, 
+            'end_time': interval_end})
+
+    def get_conditional_order_history(
+        self, 
+        market: str = None, 
+        side: str = None, 
+        type: str = None, 
+        order_type: str = None, 
+        interval_start: float = None, 
+        interval_end: float = None
+        ) -> List[dict]:
+        return self._get(f'conditional_orders/history', {
+            'market': market, 
+            'side': side, 
+            'type': type, 
+            'orderType': order_type, 
+            'start_time': interval_start, 
+            'end_time': interval_end
+            })
+
+
+    def get_conditional_orders(self, market: str = None) -> List[dict]:
+        return self._signed_get(f'conditional_orders', {'market': market})
+
+    def place_order(
+        self,
+        market: str,
+        side: str,
+        price: float,
+        size: float,
+        type_: str = 'limit',
+        reduce_only: bool = True,
+        ioc: bool = False,
+        post_only: bool = False,
+        client_id: str = None
+        ) -> dict:
+        return self._signed_post('orders',{
+            'market': market,
+            'side': side,
+            'price': price,
+            'size': size,
+            'type': type_,
+            'reduceOnly': reduce_only
+            'ioc': ioc,
+            'postOnly': post_only,
+            'clientId': client_id
+        })
         
+    def cancel_order(self, order_id: str) -> dict:
+        return self._signed_delete(f'orders/{order_id}')
+
+    """
+        MUST SPECIFY EXACTLY 1 OF THE FOLLOWING
+        order_id XOR existing_client_order_id
+    """
+
+    def modify_order(
+        self,
+        order_id: Optional[str] = None,
+        existing_client_order_id: Optional[str] = None,
+        client_order_id: Optional[float] = None,
+        price: Optional[float] = None,
+        size: Optional[float] = None,
+        ) -> dict:
+
+        assert (price is None) or (size is None), \
+            'You must modify the price or the size'
+
+        assert (existing_client_order_id is None) ^ (order_id is None), \
+            'Specify exactly one ID to modify'
+
+        if order_id:
+            endpoint_path = f"orders/{order_id}/modify"
+        elif existing_client_order_id:
+            endpoint_path = f"orders/by_client_id/{existing_client_order_id}/modify"
+
+        if size is None:
+            size_json = {}
+        else:
+            size_json = {'size': size}
+
+        if price is None:
+            price_json = {}
+        else:
+            price_json = {'price': price}
+
+        if client_order_id is None:
+            client_order_id_json = {}
+        else:
+            client_order_id_json = {'clientId': client_order_id}
+
+        return self._post(path, {
+                **(size_json),
+                **(price_json),
+                **(client_order_id_json)
+            })
+    
+    """
+    to send...
+    stop market order: set type = "stop" and specify a trigger price
+    stop limit order: also supply a limit price
+    take profit market order: set type = "trailing_stop" and specify a trigger price
+    trailin
+    """
+
+    def place_conditional_order(
+        self,
+        market: str,
+        side: str,
+        size: float,
+        type_: str = "stop",
+        limit_price: float = "None",
+        reduce_only: bool = False,
+        cancel_limit_on_trigger: bool = True,
+        trigger_price: float = None,
+        trail_value: float = None
+        ) -> dict:
+        assert type in ['stop', 'take_profit', 'trailing_stop']
+        assert type not in ['stop', 'take_profit'] or trigger_price is not None, "stop losses and take profits requires a trigger price"
+        assert type not in ['trailing stop'] or (trail_value is not None and trigger_price is not None), "must specify trail value for trailing stop, cannot use trigger price"
+        return self._signed_post(
+            'conditional_orders',
+            {
+                'market': market,
+                'side': side,
+                'triggerPrice': trigger_price
+                'size': size,
+                'reduceOnly': reduce_only,
+                'type': type_,
+                'cancelLimitOnTrigger': cancel_limit_on_trigger,
+                'orderPrice': limit_price
+            }
+        )
+
+    def get_all_trades(self, market: str, interval_start: float = None, interval_end: float = None, lim = 100) -> List:
+        
+        to_return = []
+        set_of_ids = set()
+
+        while:
+            resp = self._signed_get(f'markets/{market}/trades', {
+                'end_time': interval_end,
+                'start_time': interval_start
+            })
+        
+            dd_trades = []
+            
+            for elem in resp:
+                if elem['id'] not in set_of_ids:
+                    dd_trades.append(elem)
+            
+
+        to_return.extend(dd_trades)
+
+        set_of_ids |= {elem['id'] for elem in dd_trades}
+
+        if len(resp) == 0:
+            break
+        interval_end = min(parse_datetime(time_['time']) for time_ in resp).timestamp()
+        if limit > len(resp):
+            break
+        
+    return to_return
+
+
 
 
