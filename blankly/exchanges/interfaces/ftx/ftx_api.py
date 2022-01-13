aad9a1afaf290739663bb76e2078478e1f80bf4d
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-01-12 20:28:01 -0500

    Add licenses to FTX

--- blankly/exchanges/interfaces/ftx/ftx_api.py
+++ blankly/exchanges/interfaces/ftx/ftx_api.py
@@ -1,3 +1,22 @@
+"""
+    General FTX Api definition
+    Copyright (C) 2021 Blankly Finance
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import requests
 from typing import Optional, Dict, Any, List
 import urllib.parse
