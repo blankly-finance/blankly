420146011b1cefb030e440aafcf6923670cd1f97
qmatias <mdkotlik@gmail.com>
2022-04-04 11:52:09 -0500

    add file check

--- blankly/deployment/new_cli.py
+++ blankly/deployment/new_cli.py
@@ -22,7 +22,7 @@ TEMPLATES = {'strategy': {'none': 'none.py',
              'screener': {'none': 'none_screener.py',
                           'rsi_screener': 'rsi_screener.py'}}
 
-AUTH_URL = f'https://app.blankly.finance/auth/signin?redirectUrl=/deploy'
+AUTH_URL = 'https://app.blankly.finance/auth/signin?redirectUrl=/deploy'
 
 
 def validate_non_empty(text):
@@ -276,12 +276,7 @@ def blankly_logout(args):
         spinner.ok('Logged out')
 
 
-def blankly_deploy(args):
-    api = ensure_login()
-    # TODO ensure project exists
-
-    description = text('Enter a description for this version of the model:').unsafe_ask()
-
+def ensure_model(api: API):
     # create model if it doesn't exist
     with open('blankly.json', 'r') as file:
         data = json.load(file)
@@ -299,17 +294,38 @@ def blankly_deploy(args):
     with open('blankly.json', 'w') as file:
         json.dump(data, file, indent=4)
 
+    return data
+
+
+def missing_deployment_files() -> list:
+    paths = ['bot.py', 'blankly.json', 'keys.json', 'backtest.json', 'requirements.txt', 'settings.json']
+    return [path for path in paths if not Path(path).is_file()]
+
+
+def blankly_deploy(args):
+    api = ensure_login()
+
+    data = ensure_model(api)
+    for path in missing_deployment_files():
+        if not confirm(f'{path} is missing. Are you sure you want to continue?',
+                       default=False).unsafe_ask():
+            print_failure('Deployment cancelled')
+            print_failure(f'You can try `blankly init` to regenerate the {path} file.')
+            return
+
+    description = text('Enter a description for this version of the model:').unsafe_ask()
+
     with show_spinner('Uploading model') as spinner:
         model_path = zip_dir('.', data['ignore_files'])
 
         params = {
             'file_path': model_path,
-            'project_id': data['project_id'],
-            'model_id': data['model_id'],
+            'project_id': data['project_id'],  # set by ensure_model
+            'model_id': data['model_id'],  # set by ensure_model
             'version_description': description,
             'python_version': get_python_version(),
-            'type_': data['type'],
-            'plan': data['plan']
+            'type_': data.get('type', 'strategy'),
+            'plan': data['plan']  # set by ensure_model
         }
         if data['type'] == 'screener':
             params['schedule'] = data['screener']['schedule']
@@ -322,8 +338,8 @@ def blankly_deploy(args):
 
 
 def blankly_add_key(args):
-    exchanges = select('What exchange would you like to add a key for?', EXCHANGE_CHOICES).unsafe_ask()
-    add_key_interactive(exchanges)
+    exchange = select('What exchange would you like to add a key for?', EXCHANGE_CHOICES).unsafe_ask()
+    add_key_interactive(exchange)
 
 
 def blankly_list_key(args):
