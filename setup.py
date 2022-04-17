23583759c0bf69a8952b77897d5b34902b781b60
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-04-17 14:19:03 -0400

    v1.17.0-beta release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     name='blankly',  # How you named your package folder (MyLib)
     packages=find_packages(),
     # packages=['blankly'],  # Potentially should be the same thing as name
-    version='v1.16.20-beta',
+    version='v1.17.0-beta',
     license='lgpl-3.0',  # Licenses: https://help.github.com/articles/licensing-a-repository
     description='Rapidly build & deploy cross-exchange trading bots',  # Give a short description about your library
     long_description=long_description,
