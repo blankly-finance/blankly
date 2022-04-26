b031a7e16296edb90d3a39a9137914eec9f01a41
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-04-25 22:18:19 -0400

    Add exception for okx order size

--- blankly/exchanges/interfaces/okx/okx_interface.py
+++ blankly/exchanges/interfaces/okx/okx_interface.py
@@ -123,7 +123,6 @@ class OkxInterface(ExchangeInterface):
         response = utils.isolate_specific(needed, response)
         return MarketOrder(order, response, self)
 
-
     @utils.order_protection
     def limit_order(self, symbol, side, price, size) -> LimitOrder:
         """
@@ -136,7 +135,9 @@ class OkxInterface(ExchangeInterface):
         """
         needed = self.needed['limit_order']
         if self.should_auto_trunc:
-            size = utils.trunc(size, self.get_asset_precision(symbol))
+            result = utils.trunc(size, self.get_asset_precision(symbol))
+            if result != 0:
+                size = result
         order = {
             'symbol': symbol,
             'side': side,
