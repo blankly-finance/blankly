270bd27a61557443d07b32bca12e2b0e96a583b7
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-05-03 11:21:13 -0400

    Add keyless backtest example

---
+++ examples/keyless_backtest.py
@@ -0,0 +1,44 @@
+import requests
+import blankly
+from blankly.data import PriceReader
+
+
+def price_event(price, symbol, state: blankly.StrategyState):
+    """ This function will give an updated price every 15 seconds from our definition below """
+    state.variables['history'].append(price)
+    rsi = blankly.indicators.rsi(state.variables['history'])
+    curr_value = state.interface.account[state.base_asset].available
+    if rsi[-1] < 30 and not curr_value:
+        # Dollar cost average buy
+        buy = state.interface.cash / price
+        state.interface.market_order(symbol, side='buy', size=blankly.trunc(buy, 5))
+    elif rsi[-1] > 70 and curr_value:
+        # Dollar cost average sell
+        state.interface.market_order(symbol, side='sell', size=blankly.trunc(curr_value, 5))
+
+
+def init(symbol, state: blankly.StrategyState):
+    # Download price data to give context to the algo
+    state.variables['history'] = state.interface.history(symbol, to=150, return_as='deque',
+                                                         resolution=state.resolution)['close']
+
+
+if __name__ == "__main__":
+    # This downloads an example CSV
+    data = requests.get(
+        'https://firebasestorage.googleapis.com/v0/b/blankly-6ada5.appspot.com/o/demo_data.csv?alt=media&token=acfa5c39-8f08-45dc-8be3-2033dc2b7b28').text
+    with open('./price_examples.csv', 'w') as file:
+        file.write(data)
+
+    # Run on the keyless exchange, starting at 100k
+    exchange = blankly.KeylessExchange(price_reader=PriceReader('./price_examples.csv', 'BTC-USD'))
+
+    # Use our strategy helper
+    strategy = blankly.Strategy(exchange)
+
+    # Make the price event function above run every day
+    strategy.add_price_event(price_event, symbol='BTC-USD', resolution='1d', init=init)
+
+    # Backtest the strategy
+    results = strategy.backtest(start_date=1588377600, end_date=1650067200, initial_values={'USD': 10000})
+    print(results)
