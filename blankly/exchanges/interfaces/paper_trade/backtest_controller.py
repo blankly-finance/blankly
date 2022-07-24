334bd624b73bcf8c905ab8707d77c7ddd16d66f4
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-07-24 10:18:30 -0400

    Update sample link

--- blankly/exchanges/interfaces/paper_trade/backtest_controller.py
+++ blankly/exchanges/interfaces/paper_trade/backtest_controller.py
@@ -1158,7 +1158,8 @@ class BackTestController(ABCBacktestController):  # circular import to type mode
 
                 show(bokeh_columns(figures))
                 info_print(f'Make an account to take advantage of the platform backtest viewer: '
-                           f'https://app.blankly.finance/RETIe0J8EPSQz7wizoJX0OAFb8y1/EZkgTZMLJVaZK6kNy0mv/'
+                           f'https://app.blankly.finance/RETIe0J8EPSQz7wizoJX0OAFb8y1/62iIMVRKV7zkcpJysYlP/'
+                           f'75a0c190-4d8a-44e2-9310-c47d4d72b070/backtest'
                            f'2b2ff92c-ee41-42b3-9afb-387de9e4f894/backtest')
 
             # This is where we end the backtesting time
